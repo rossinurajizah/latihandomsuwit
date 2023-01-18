08fe7b4237aa89b30eeb8058327e69ddaaf96cbe
rossinurajizah <122016207+rossinurajizah@users.noreply.github.com>
2023-01-19 06:53:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function getPilihanComputer () {
+const comp = Math.random();
+
+    if( comp < 0.34 ) return 'gajah';
+    if( comp >= 0.34 && comp < 0.67 ) return 'orang';
+    return 'semut';
+    }
+    
+    function getHasil(comp,player) {
+    if( player == comp ) return 'SERI!';
+    if( player == 'gajah' ) return ( comp == 'orang' ) ? ' MENANG:)' : 'KALAH:(';
+    if( player == 'orang' ) return ( comp == 'gajah' ) ? 'KALAH:(' : 'MENANG:)';
+    if( player == 'semut' ) return ( comp == 'orang' ) ? 'KALAH:(' : 'MENANG:)';
+    }
+    const pilihan=document.querySelectorAll('li img');
+    pilihan.forEach(function(pil) {
+      pil.addEventListener('click',function() {
+     const pilihanComputer=getPilihanComputer ();
+        const pilihanPlayer=pil.className;
+        const hasil=getHasil(pilihanComputer,pilihanPlayer);
+        
+        const imgComputer=document.querySelector('.img-komputer');
+        imgComputer.setAttribute('src','img/' +pilihanComputer + '.png');
+        
+        const info =document.querySelector('.info');
+        info.innerHTML =hasil;
+      });
+    });
